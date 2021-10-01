6c95fc3e53d0b8612f80d46775f72f2a2e62d3af
Cedro <yotechgirl@gmail.com>
2021-10-01 10:45:12 -0300

    feat: get,post,path,put,delete

--- src/index.js
+++ src/index.js
@@ -14,16 +14,44 @@ const app = express();
 
 app.get('/courses', (request, response) => {
   return response.json([
-    'O Poderoso Chefão. 2h 55min / Policial, Drama',
-    'A Lista de Schindler. 3h 15min / Histórico, Guerra',
-    'Um Sonho de Liberdade. 2h 20min / Drama. ...',
-    'O Rei Leão. 1h 29min / Aventura, Animação, Musical. ...',
-    'Forrest Gump - O Contador de Histórias. ...',
-    'O Senhor dos Anéis - O Retorno do Rei. ...',
-    ' Vingadores: Ultimato...',
-    'À Espera de um Milagre.',
-    'Matrix - / Ação / 1h 29min',
+    'Course 1',
+    'Course 2',
+    'Course 3',
+    'Course 4',
+    'Course 5',
   ]);
+});
+
+app.post('/courses', (request, response) => {
+  return response.json(
+    'Course 1',
+    'Course 2',
+    'Course 3',
+    'Course 4',
+    'Course 5',
+    'Course 6',
+    'Course 7',
+  );
+});
+
+app.patch('/courses', (request, response) => {
+  return response.json(
+    'Course 1',
+    'Course 2',
+    'Course 3',
+    'Course 4',
+    'Course 5',
+    'Course 9',
+    'Course 7',
+  );
+});
+
+app.put('/courses/:id', (request, response) => {
+  return response.json('Course 1', 'Course 1', 'Course 1', 'Course 1');
+});
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json("'Course 1','Course 1','Course 1','Course 1'");
 });
 
 app.listen(3333);
